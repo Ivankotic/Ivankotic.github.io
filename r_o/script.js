a73116dee6bc4636ed402ac436e8b9ba75c2f455
Ivankotic <39311604+Ivankotic@users.noreply.github.com>
2019-12-06 17:44:52 +0500

    Add files via upload

---
+++ r_o/script.js
@@ -0,0 +1,21 @@
+function f() {
+    var v = $('#vvod').val();
+
+    if (v == "") {
+        v = 0;
+    }
+    else {
+        v = v.split("");
+        var l = v.length;
+        var sum=0;
+        for(var i=0;i<l;i++){
+            sum = sum + parseInt(v[i]);
+        }
+        
+        v = sum / l;
+    }
+
+    $('#o').html(v);
+}
+
+setInterval(f, 100)

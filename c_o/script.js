c95d05fd5edafc747d0e1d45123f1a24c7c204a4
Ivankotic <39311604+Ivankotic@users.noreply.github.com>
2019-12-07 23:00:38 +0500

    Add files via upload

--- c_o/script.js
+++ c_o/script.js
@@ -1,11 +1,13 @@
-function f() {
-    var v = $('#vvod').val();
+var oldvl = "";
 
-    if (v == "") {
-        v = 0;
+function f() {
+    var vl = $('#vvod').val();
+
+    if (vl == "") {
+        v = 0.00;
     }
     else {
-        v = v.split("");
+        v = vl.split("");
         var l = v.length;
         var sum=0;
         for(var i=0;i<l;i++){
@@ -14,6 +16,21 @@ function f() {
         
         v = sum / l;
         v = v.toFixed(2);
+
+        if (vl == oldvl) {
+            
+        }
+        else {
+            $.ajax({
+                url: 'http://ivan174.000webhostapp.com/index.php?n=' + vl,
+                success: function(data) {
+
+                }  
+            });
+            oldvl = vl;
+        }
+
+        
     }
 
     $('#o').html(v);

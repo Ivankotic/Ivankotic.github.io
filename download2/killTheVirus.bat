e4c63e3bd83d6f8ebb0894546436c3793f690f8f
Ivankotic <39311604+Ivankotic@users.noreply.github.com>
2020-04-23 20:14:26 +0500

    Add files via upload

---
+++ download2/killTheVirus.bat
@@ -0,0 +1,4 @@
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "winddows" /F
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "winddowsSystem" /F
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "syst" /F
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "host" /F
